b19f3e73a024130eacfe4d7bb1da3387d0bf1d18
Zhorin Sergey <ser4038772@mail.ru>
2015-11-15 20:07:34 +0300

    Winter Is Coming

--- gtest/MyList-test/test_MyList.cpp
+++ gtest/MyList-test/test_MyList.cpp
@@ -24,10 +24,10 @@ TEST(MyList, CreateList) {
   }
   EXPECT_EQ(kListSize, k);
 }
-TEST(MyList, functionDel) {
+TEST(MyList, can_del_first_element) {
   const int kListSize = 5;
   int vals[kListSize];
-  int n = 2;
+  int n = 5;
   for (int i = 0; i < 5; ++i)
     vals[i] = i + 1;
   CNode *p = CreateList(kListSize, vals);
@@ -45,4 +45,53 @@ TEST(MyList, functionDel) {
   CNode *e = del(&p, n);
   print(e);
   EXPECT_EQ(true, comp(b, e));
+}
+TEST(MyList, can_del_last_element) {
+  const int kListSize = 5;
+  int vals[kListSize];
+  int n = 1;
+  for (int i = 0; i < 5; ++i)
+    vals[i] = i + 1;
+  CNode *p = CreateList(kListSize, vals);
+  for (int i = 0; i < 4; ++i) {
+    if (i >= (5 - n))
+      vals[i] = i + 2;
+    else
+      vals[i] = i + 1;
+  }
+  CNode *b = CreateList(kListSize - 1, vals);
+  print(p);
+  cout << endl;
+  print(b);
+  cout << endl;
+  CNode *e = del(&p, n);
+  print(e);
+  EXPECT_EQ(true, comp(b, e));
+}
+TEST(MyList, cant_del_element_with_too_large_itdex) {
+  const int kListSize = 5;
+  int vals[kListSize];
+  int n = 6;
+  for (int i = 0; i < 5; ++i)
+    vals[i] = i + 1;
+  CNode *p = CreateList(kListSize, vals);
+  ASSERT_ANY_THROW(del(&p, n));
+}
+TEST(MyList, cant_del_element_with_negative_itdex) {
+  const int kListSize = 5;
+  int vals[kListSize];
+  int n = -6;
+  for (int i = 0; i < 5; ++i)
+    vals[i] = i + 1;
+  CNode *p = CreateList(kListSize, vals);
+  ASSERT_ANY_THROW(del(&p, n));
+}
+TEST(MyList, cant_del_element_from_empty_node) {
+  const int kListSize = 5;
+  int vals[kListSize];
+  int n = 2;
+  for (int i = 0; i < 5; ++i)
+    vals[i] = i + 1;
+  CNode *p = CreateList(0, vals);
+  ASSERT_ANY_THROW(del(&p, n));
 }
